23029f57d0738e95d29e51f91dfd3d9b62a141a1
Satheesh C <satheesancchandran@gmail.com>
2020-06-10 19:51:33 +0530

    Implemented bfs

--- src/graph.js
+++ src/graph.js
@@ -1,11 +1,31 @@
-//Example 
+//Example
 // Pairs => [[from,to],[to,from]]
 // Source => from
-// To => to 
+// To => to
 // Should return true.
 
-const bfs = function(pairs,source,target){
-  
+const findEdges = function (pairs, root) {
+  const edges = [];
+  pairs.forEach(pair => {
+    if (pair[0] == root) edges.push(pair[1]);
+  });
+  return edges;
 };
 
-module.exports = {bfs};
+const bfs = function (pairs, source, target) {
+  const queue = [];
+  const visitedList = [];
+  queue.push(source);
+  while (queue.length !== 0) {
+    const node = queue.shift();
+    if (node == target) return true;
+    visitedList.push(node);
+    const edges = findEdges(pairs, node);
+    edges.forEach(edge => {
+      if (!visitedList.includes(edge)) queue.push(edge);
+    });
+  }
+  return false;
+};
+
+module.exports = { bfs };
